fcdf9a8b87c910463272073781fae4a7c85630c0
Ramya Deepak <121786057+RDLN1687@users.noreply.github.com>
2023-02-02 23:07:49 -0700

    Add files via upload

---
+++ Project1_expressions_alert/JS/main.js
@@ -0,0 +1,8 @@
+window.alert ("It is a going to be a great!") //Window alert
+document.write ("New Day!") // text
+
+var Sent1 = "This is the beginning of the string"; //2 variables
+var Sent2 = "and this is the end of the string";
+document.write (Sent1+Sent2) //Concatenating variables
+
+document.write(3.87*4.5) // expression
